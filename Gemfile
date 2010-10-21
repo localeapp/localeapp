f29cd4b08683e2e9483522e7b4e9d42714b7b75d
Christopher Dell <chris@tigrish.com>
2010-10-21 18:35:39 +0200

    Sender posts json requests using RestClient

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in olba.gemspec
 gemspec
+json
+rest-client
--- lib/olba/sender.rb
+++ lib/olba/sender.rb
@@ -1,21 +1,18 @@
-require 'net/http'
-require 'uri'
+require 'rest-client'
+require 'json'
 
 module Olba
   class Sender
-    
+
     def post_translation(locale, key, options)
-      data = {
-        :api_key  => Olba.configuration.api_key,
-        :locale   => locale,
-        :key      => key,
-        :options  => options }
-      response = Net::HTTP.post_form(URI.parse(translation_resource_url), data)
-      Olba.log([translation_resource_url, response.code, data.inspect].join(' - '))
+      data = { :api_key => Olba.configuration.api_key, :locale => locale, :key => key, :options => options }
+      RestClient.post(translation_resource_url, data.to_json, :content_type => :json, :accept => :json) do |response, request, result|
+        Olba.log([translation_resource_url, response.code, data.inspect].join(' - '))
+      end
     end
 
     def translation_resource_url
-      "http://#{Olba.configuration.host}/translations"
+      "http://#{Olba.configuration.host}:#{Olba.configuration.port}/translations"
     end
 
   end

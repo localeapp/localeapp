8562a0fd4a168b1ae374a3bf7db4adb900b99994
Christopher Dell <chris@tigrish.com>
2010-10-28 18:42:34 +0200

    Temporarily remove t => I18n.translate alias (rails 3)

--- lib/olba/rails/translation_helper.rb
+++ lib/olba/rails/translation_helper.rb
@@ -1,10 +1,10 @@
 module ActionView
   module Helpers
     module TranslationHelper
-      def translate(key, options = {})
-        I18n.translate(key, options)
-      end
-      alias :t :translate
+#      def translate(key, options = {})
+#        I18n.translate(key, options)
+#      end
+#      alias :t :translate
     end
   end
 end

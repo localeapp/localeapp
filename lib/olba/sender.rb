cc59473fe51fb8c2df47a85a37546aee00496d2d
Christopher Dell <chris@tigrish.com>
2010-11-05 18:33:14 +0100

    Added an option to Sender#post_translation so that values can be submitted

--- lib/olba/sender.rb
+++ lib/olba/sender.rb
@@ -4,8 +4,8 @@ require 'json'
 module Olba
   class Sender
 
-    def post_translation(locale, key, options)
-      translation = { :key => key, :locale => locale, :substitutions => options.keys }
+    def post_translation(locale, key, options={}, value=nil)
+      translation = { :key => key, :locale => locale, :substitutions => options.keys, :description => value}
       data = { :api_key => Olba.configuration.api_key, :translation => translation }
       RestClient.post(translation_resource_url, data.to_json, :content_type => :json, :accept => :json) do |response, request, result|
         Olba.log([translation_resource_url, response.code, data.inspect].join(' - '))

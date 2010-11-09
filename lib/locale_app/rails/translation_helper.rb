e5d4435c83aa86df3a5df42141378de33c845546
Chris McGrath <chris@octopod.info>
2010-11-09 12:52:49 +0000

    Revert "Remove ActionView monkey patch"
    Is actually needed as rails overwrites the I18n exception handler in
    ActionView::TranslateHelper#translate
    
    This reverts commit 43cb05eb2353d5ec79890abb2dea4e0c38bb4997.

---
+++ lib/locale_app/rails/translation_helper.rb
@@ -0,0 +1,10 @@
+module ActionView
+  module Helpers
+    module TranslationHelper
+      def translate(key, options = {})
+        I18n.translate(key, options)
+      end
+      alias :t :translate
+    end
+  end
+end

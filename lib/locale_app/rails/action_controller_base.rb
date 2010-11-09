31370539c428cdebedff171e92447c96a8119203
Chris McGrath <chris@octopod.info>
2010-11-09 11:08:01 +0000

    Change some puts to log

--- lib/locale_app/rails/action_controller_base.rb
+++ lib/locale_app/rails/action_controller_base.rb
@@ -1,20 +1,20 @@
 module ActionController
   class Base
     def handle_translation_updates
-      puts Time.now.to_i.to_s << '-- Handling translation updates'
+      ::LocaleApp.log Time.now.to_i.to_s << '-- Handling translation updates'
       # ask the server for new translations
       if ::LocaleApp.poller.needs_polling?
-        puts Time.now.to_i.to_s << ' - polling'
+        ::LocaleApp.log Time.now.to_i.to_s << ' - polling'
         ::LocaleApp.poller.poll!
         if ::LocaleApp.poller.cluster_updated?
-          puts Time.now.to_i.to_s << '- downloading translations'
+          ::LocaleApp.log Time.now.to_i.to_s << '- downloading translations'
           ::LocaleApp.poller.get_translations!
         end
       end
        
       # reload i18n when new translations have been downloaded
       if ::LocaleApp.poller.cluster_updated?
-        puts Time.now.to_i.to_s << '- reloading I18n'
+        ::LocaleApp.log Time.now.to_i.to_s << '- reloading I18n'
         I18n.reload!
         ::LocaleApp.poller.updated_at = ::LocaleApp.poller.cluster_log[:updated_at]
       end

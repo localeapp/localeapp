a6483d9788496702d3d5229fbec9c7530093002e
Chris McGrath <chris@octopod.info>
2010-11-09 11:19:11 +0000

    Remove and rename some configuration options

--- lib/locale_app/configuration.rb
+++ lib/locale_app/configuration.rb
@@ -10,12 +10,6 @@ module LocaleApp
     # The port to connect to (defaults to 80)
     attr_accessor :port
 
-    # The HTTP open timeout in seconds (defaults to 2).
-    attr_accessor :http_open_timeout
-
-    # The HTTP read timeout in seconds (defaults to 5).
-    attr_accessor :http_read_timeout
-
     # The name of the environment the application is running in
     attr_accessor :environment_name
 
@@ -25,7 +19,7 @@ module LocaleApp
 
     # The names of environments where notifications aren't sent (defaults to
     # 'test', 'cucumber')
-    attr_accessor :development_environments
+    attr_accessor :disabled_environments
 
     # The logger used by LocaleApp
     attr_accessor :logger
@@ -47,7 +41,7 @@ module LocaleApp
       @port                      = 80
       @http_open_timeout         = 2
       @http_read_timeout         = 5
-      @development_environments  = %w(test cucumber)
+      @disabled_environments     = %w(test cucumber)
       @poll_interval             = 60
       @synchronization_data_file = 'locale_app.yml'
       @locale_file               = 'translations.yml'

335d3fcd2ac46a36bacb73e4278e082a3f3cffdf
Chris McGrath <chris@octopod.info>
2010-11-09 11:26:21 +0000

    Use new method names

--- lib/locale_app/rails/action_controller_base.rb
+++ lib/locale_app/rails/action_controller_base.rb
@@ -6,14 +6,14 @@ module ActionController
       if ::LocaleApp.poller.needs_polling?
         ::LocaleApp.log Time.now.to_i.to_s << ' - polling'
         ::LocaleApp.poller.poll!
-        if ::LocaleApp.poller.cluster_updated?
+        if ::LocaleApp.poller.translations_changed?
           ::LocaleApp.log Time.now.to_i.to_s << '- downloading translations'
           ::LocaleApp.poller.get_translations!
         end
       end
        
       # reload i18n when new translations have been downloaded
-      if ::LocaleApp.poller.cluster_updated?
+      if ::LocaleApp.poller.translations_changed?
         ::LocaleApp.log Time.now.to_i.to_s << '- reloading I18n'
         I18n.reload!
         ::LocaleApp.poller.updated_at = ::LocaleApp.poller.synchronization_data[:updated_at]

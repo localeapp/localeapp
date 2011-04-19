a85a590d12fd5a868367517b172ffc706002074c
Christopher Dell <chris@tigrish.com>
2011-04-19 16:11:41 +0200

    LocaleApp::Routes#translations_url accepts a :query hash

--- spec/locale_app/routes_spec.rb
+++ spec/locale_app/routes_spec.rb
@@ -4,32 +4,38 @@ class TestRoutes
   include LocaleApp::Routes
 end
   
-describe LocaleApp::Routes, '#project_url' do
+describe LocaleApp::Routes do
   before(:each) do
     @routes = TestRoutes.new
+    @config = {:host => 'test.host', :api_key => 'API_KEY'}
   end
 
-  it "is constructed from the configuration host and port" do
-    with_configuration(:host => 'test.host', :port => 1234, :api_key => 'API_KEY') do
-      @routes.project_url.should == "http://test.host:1234/api/projects/API_KEY"
+  describe '#project_url' do
+    it "is constructed from the configuration host and port" do
+      with_configuration(@config.merge(:port => 1234)) do
+        @routes.project_url.should == "http://test.host:1234/api/projects/API_KEY"
+      end
+    end
+
+    it "includes http auth if in configuration" do
+      with_configuration(@config.merge(:port => 1234, :http_auth_username => 'foo', :http_auth_password => 'bar')) do
+        @routes.project_url.should == "http://foo:bar@test.host:1234/api/projects/API_KEY"
+      end
     end
   end
 
-  it "includes http auth if in configuration" do
-    with_configuration(:host => 'test.host', :port => 1234, :http_auth_username => 'foo', :http_auth_password => 'bar', :api_key => 'API_KEY') do
-      @routes.project_url.should == "http://foo:bar@test.host:1234/api/projects/API_KEY"
+  describe "#translations_url" do
+    it "it extends the project_url" do
+      with_configuration(@config) do
+        @routes.translations_url.should == "http://test.host/api/projects/API_KEY/translations"
+      end
+    end
+
+    it "adds query parameters on to the url" do
+      with_configuration(@config) do
+        @routes.translations_url(:query => {:updated_at => '2011-04-19', :foo => :bar}).should == "http://test.host/api/projects/API_KEY/translations?updated_at=2011-04-19&foo=bar"
+      end
     end
   end
-end
 
-describe LocaleApp::Routes, "#translations_url" do
-  before(:each) do
-    @routes = TestRoutes.new
-  end
-
-  it "it extends the project_url" do
-    with_configuration(:host => 'test.host', :port => 1234, :api_key => 'API_KEY') do
-      @routes.translations_url.should == "http://test.host:1234/api/projects/API_KEY/translations"
-    end
-  end
 end

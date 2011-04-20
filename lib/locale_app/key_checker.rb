3f20948699c993a3319157589170fe6b42611a30
Chris McGrath <chris@octopod.info>
2011-04-20 11:50:25 +0100

    Teach KeyChecker about Routes

--- lib/locale_app/key_checker.rb
+++ lib/locale_app/key_checker.rb
@@ -4,29 +4,19 @@ require 'time'
 
 module LocaleApp
   class KeyChecker
+    include ::LocaleApp::Routes
+
     def check(key)
       begin
-        response = RestClient.get(project_resource_url(key))
+        if LocaleApp.configuration.nil? # no config file yet
+          LocaleApp.configuration = LocaleApp::Configuration.new
+        end
+        LocaleApp.configuration.api_key = key
+        response = RestClient.get(project_url + ".json")
         [true, JSON.parse(response)]
       rescue RestClient::ResourceNotFound => e
         [false, {}]
       end
-    end
-
-    private
-    def project_resource_url(key)
-      if LocaleApp.configuration.nil? # no config file yet
-        LocaleApp.configuration = LocaleApp::Configuration.new
-      end
-      uri_params = {
-        :host => LocaleApp.configuration.host,
-        :port => LocaleApp.configuration.port,
-        :path => "/projects/#{key}.json"
-      }
-      if LocaleApp.configuration.http_auth_username
-        uri_params[:userinfo] = "#{LocaleApp.configuration.http_auth_username}:#{LocaleApp.configuration.http_auth_password}"
-      end
-      URI::HTTP.build(uri_params).to_s
     end
   end
 end

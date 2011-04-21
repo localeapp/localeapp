a8b135ba2431a6d879060ccf75bf39cba395695f
Chris McGrath <chris@octopod.info>
2011-04-21 12:47:13 +0100

    Use explicit formats when calling api
    From http://weblog.rubyonrails.org/2011/4/21/jquery-new-default
    
    "In particular, if your application uses jQuery the default Accept
    header in the test will be different from the Accept header sent by
    jQuery. That's some vinegar: Rails encourages you to base interfaces on
    explicit format parameters. So, for example, best practice is to provide
    a /users.json endpoint, rather than a /users endpoint with a JSON
    representation chosen depending on the Accept header."
    
    So lets not go off the rails golden path when it's easy to stay on it

--- spec/locale_app/routes_spec.rb
+++ spec/locale_app/routes_spec.rb
@@ -11,31 +11,42 @@ describe LocaleApp::Routes do
   end
 
   describe '#project_url' do
-    it "is constructed from the configuration host and port" do
+    it "is constructed from the configuration host and port and defaults to json" do
       with_configuration(@config.merge(:port => 1234)) do
-        @routes.project_url.should == "http://test.host:1234/projects/API_KEY"
+        @routes.project_url.should == "http://test.host:1234/projects/API_KEY.json"
       end
     end
 
     it "includes http auth if in configuration" do
       with_configuration(@config.merge(:port => 1234, :http_auth_username => 'foo', :http_auth_password => 'bar')) do
-        @routes.project_url.should == "http://foo:bar@test.host:1234/projects/API_KEY"
+        @routes.project_url.should == "http://foo:bar@test.host:1234/projects/API_KEY.json"
+      end
+    end
+
+    it "can be changed to another content type" do
+      with_configuration(@config) do
+        @routes.project_url(:format => :yml).should == 'http://test.host/projects/API_KEY.yml'
       end
     end
   end
 
   describe "#translations_url" do
-    it "it extends the project_url" do
+    it "it extends the project_url and defaults to json" do
       with_configuration(@config) do
-        @routes.translations_url.should == "http://test.host/projects/API_KEY/translations"
+        @routes.translations_url.should == "http://test.host/projects/API_KEY/translations.json"
       end
     end
 
     it "adds query parameters on to the url" do
       with_configuration(@config) do
-        @routes.translations_url(:query => {:updated_at => '2011-04-19', :foo => :bar}).should == "http://test.host/projects/API_KEY/translations?updated_at=2011-04-19&foo=bar"
+        @routes.translations_url(:query => {:updated_at => '2011-04-19', :foo => :bar}).should == "http://test.host/projects/API_KEY/translations.json?updated_at=2011-04-19&foo=bar"
+      end
+    end
+
+    it "can be changed to another content type" do
+      with_configuration(@config) do
+        @routes.translations_url(:format => :yml).should == 'http://test.host/projects/API_KEY/translations.yml'
       end
     end
   end
-
 end

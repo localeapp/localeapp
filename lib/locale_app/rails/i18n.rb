4d23cd56bdf998c9751a281273bd6658cb75be4a
Chris McGrath <chris@octopod.info>
2011-04-21 16:25:39 +0100

    I18n.exception_handler tweaks
      * Rename to locale_app_exception_handler
      * just plain raise will re-raise the exception

--- lib/locale_app/rails/i18n.rb
+++ lib/locale_app/rails/i18n.rb
@@ -1,6 +1,6 @@
 module I18n
   class << self
-    def obla_exception_handler(exception, locale, key, options)
+    def locale_app_exception_handler(exception, locale, key, options)
       LocaleApp.log(exception.message)
       if MissingTranslationData === exception
         LocaleApp.log('Detected missing translation')
@@ -8,11 +8,11 @@ module I18n
         [locale, key].join(', ')
       else
         LocaleApp.log('Raising exception')
-        raise exception
+        raise
       end
     end
 
   end
 end
 
-I18n.exception_handler = :obla_exception_handler
+I18n.exception_handler = :locale_app_exception_handler

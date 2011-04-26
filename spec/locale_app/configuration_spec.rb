39a5f18d5ebff066f4e18b0d74ed1606d92aac49
Christopher Dell <chris@tigrish.com>
2011-04-26 14:30:47 +0200

    Set the default configuration : poll_interval is 0; development environment is enabled; production environment is disabled. Refs #4284

--- spec/locale_app/configuration_spec.rb
+++ spec/locale_app/configuration_spec.rb
@@ -30,6 +30,46 @@ describe LocaleApp::Configuration do
     @configuration.translation_data_directory = "test"
     @configuration.translation_data_directory.should == "test"
   end
+
+  context "environment" do
+    it "defaults development to enabled" do
+      @configuration.environment_name = 'development'
+      @configuration.should_not be_disabled
+    end
+
+    it "defaults cucumber to disabled" do
+      @configuration.environment_name = 'cucumber'
+      @configuration.should be_disabled
+    end
+
+    it "defaults test to disabled" do
+      @configuration.environment_name = 'test'
+      @configuration.should be_disabled
+    end
+
+    it "defaults production to disabled" do
+      @configuration.environment_name = 'production'
+      @configuration.should be_disabled
+    end
+  end
+end
+  
+describe LocaleApp::Configuration, '#disabled?' do
+  before(:each) do
+    @configuration = LocaleApp::Configuration.new
+  end
+
+  it "returns true when environemnt_name is in disableld_environments" do
+    @configuration.disabled_environments = %w{foo bar}
+    @configuration.environment_name = 'foo'
+    @configuration.disabled?.should be_true
+  end
+
+  it "returns false when environemnt_name is NOT in disableld_environments" do
+    @configuration.disabled_environments = %w{foo bar}
+    @configuration.environment_name = 'biz'
+    @configuration.disabled?.should be_false
+  end
 end
 
 describe LocaleApp::Configuration, "#write_initial(path)" do

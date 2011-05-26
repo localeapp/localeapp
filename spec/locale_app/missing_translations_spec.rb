5ed26cf94b83ae32a79ebad58159a31344feffd7
Chris McGrath <chris@octopod.info>
2011-05-26 11:31:15 +0100

    Add Sender#post_missing_translations method
      * Leaving the post_translation there for now till we decide what API
        we want.
      * Added MissingTranslations#to_send, which is dumb for now but will
        get cleverer

--- spec/locale_app/missing_translations_spec.rb
+++ spec/locale_app/missing_translations_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 require 'locale_app/missing_translations'
 
-describe LocaleApp::MissingTranslations, "#add(locale, key, options)" do
+describe LocaleApp::MissingTranslations, "#add(locale, key, options = {})" do
   it "stores the missing translation data" do
     translations = LocaleApp::MissingTranslations.new
     translations.add(:en, 'foo', { :baz => 'bam' })
     translations[:en].should include('foo')
     translations[:en]['foo'].options.should == { :baz => 'bam' }
+  end
+end
+
+describe LocaleApp::MissingTranslations, "#to_send" do
+  it "returns an array of missing translation data that needs to be sent to localeapp.com" do
+    translations = LocaleApp::MissingTranslations.new
+    translations.add(:en, 'foo', { :baz => 'bam' })
+    translations.add(:es, 'bar')
+
+    translations.to_send.should == [
+      { :key => 'foo', :locale => :en, :options => { :baz => 'bam' } },
+      { :key => 'bar', :locale => :es, :options => {}}
+    ]
   end
 end

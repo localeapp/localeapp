e9bf55c1aaff9a4d8ccff68a593143ed615577b2
Chris McGrath <chris@octopod.info>
2011-06-01 10:27:31 +0200

    Install exits with non-zero status if check fails
    This is to make it easier to script the integration test

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -7,6 +7,12 @@ When /^I have a valid project on localeapp\.com with api key "([^"]*)"$/ do |api
   add_fake_web_uri(:post, "http://api.localeapp.com/projects/#{api_key}/import/", ['202', 'OK'], '')
 end
 
+When /^I have a valid project on localeapp\.com but an incorrect api key "([^"]*)"$/ do |bad_api_key|
+  uri = "http://api.localeapp.com/projects/#{bad_api_key}.json"
+  body = valid_project_data.to_json
+  add_fake_web_uri(:get, uri, ['404', 'Not Found'], body)
+end
+
 When /^I have a translations on localeapp\.com for the project with api key "([^"]*)"$/ do |api_key|
   uri = "http://api.localeapp.com/projects/#{api_key}/translations.json"
   body = valid_translation_data.to_json

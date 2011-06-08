97b3523aa4848d7514014f810cff2b6e4adf3531
Chris McGrath <chris@octopod.info>
2011-06-08 14:16:14 +0200

    Print a message if there are no updates to

--- lib/locale_app/cli/update.rb
+++ lib/locale_app/cli/update.rb
@@ -10,6 +10,8 @@ module LocaleApp
         @output.puts("LocaleApp update: checking for translations since #{poller.updated_at}")
         if poller.poll!
           @output.puts "Found and updated new translations"
+        else
+          @output.puts "No new translations"
         end
       end
     end

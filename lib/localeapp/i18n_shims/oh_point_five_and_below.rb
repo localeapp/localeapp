d498f4bf213c05c54d0e3a2078234410abf3d15c
Chris McGrath <chris@octopod.info>
2011-08-24 20:16:59 +0100

    Setup for travis
      * Gemfiles for i18n 0.5.0 and 0.6.0
      * Make i18n a runtime not dev dependency
      * Some temporary output to help setup Travis

--- lib/localeapp/i18n_shims/oh_point_five_and_below.rb
+++ lib/localeapp/i18n_shims/oh_point_five_and_below.rb
@@ -1,1 +1,2 @@
+puts "Using i18n <= 0.5.0 Shim" # temporary till we know Travis is setup properly
 Localeapp::I18nMissingTranslationException = I18n::MissingTranslationData
--- lib/localeapp/i18n_shims/oh_point_six_and_above.rb
+++ lib/localeapp/i18n_shims/oh_point_six_and_above.rb
@@ -1,1 +1,2 @@
+puts "Using i18n >= 0.6.0 Shim" # temporary till we know Travis is setup properly
 Localeapp::I18nMissingTranslationException = I18n::MissingTranslation

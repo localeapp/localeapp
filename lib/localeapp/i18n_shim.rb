563cbf8be9df09e0e518f9423e9a1488bb3b5ae4
Chris McGrath <chris@octopod.info>
2011-08-25 09:23:12 +0100

    Remove puts used for checking travis

--- lib/localeapp/i18n_shim.rb
+++ lib/localeapp/i18n_shim.rb
@@ -1,7 +1,5 @@
 if defined? I18n::MissingTranslation 
-  puts "Using i18n >= 0.6.0" # temporary till we know Travis is setup properly
   Localeapp::I18nMissingTranslationException = I18n::MissingTranslation
 else
-  puts "Using i18n <= 0.5.0" # temporary till we know Travis is setup properly
   Localeapp::I18nMissingTranslationException = I18n::MissingTranslationData
 end

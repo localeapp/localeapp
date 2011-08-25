a1bda49eba74317040194859c34bdc0b7f6a0e0b
Oriol Gual <oriol.gual@gmail.com>
2011-08-25 04:12:18 +0300

    add support for I18n::MissingTranslation when handling exceptions (see https://github.com/svenfuchs/i18n/commit/9d85541a5ed34a2afca8e49715945ed0b94a2935)

--- lib/localeapp/exception_handler.rb
+++ lib/localeapp/exception_handler.rb
@@ -2,7 +2,7 @@ module Localeapp
   class ExceptionHandler
     def self.call(exception, locale, key, options)
       Localeapp.log(exception.message)
-      if I18n::MissingTranslationData === exception
+      if [I18n::MissingTranslationData, I18n::MissingTranslation].include?(exception.class)
         Localeapp.log("Detected missing translation for key(s) #{key.inspect}")
 
         [*key].each do |key|

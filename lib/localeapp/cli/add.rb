59515e5e16bcb0cc13536d45efd40fc531177a37
Chris McGrath <chris@octopod.info>
2011-09-14 10:52:58 +0100

    Better handling of badly formed add args

--- lib/localeapp/cli/add.rb
+++ lib/localeapp/cli/add.rb
@@ -9,8 +9,13 @@ module Localeapp
         @output.puts "Localeapp Add"
         @output.puts ""
         translations.each do |translation|
-          locale, description = translation.split(/:/)
-          Localeapp.missing_translations.add(locale, key, description)
+          if translation =~ /([\w\-]+):(.*)/
+            locale, description = $1, $2
+            Localeapp.missing_translations.add(locale, key, description)
+          else
+            @output.puts "Ignoring bad translation #{translation}"
+            @output.puts "format should be <locale>:<translation content>"
+          end
         end
         @output.puts "Sending key: #{key}"
         Localeapp.sender.post_missing_translations
--- spec/localeapp/cli/add_spec.rb
+++ spec/localeapp/cli/add_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe Localeapp::CLI::Add, "#execute(key, *translations)" do
-  def do_action
-    @command.execute('test.key', 'en:test en', 'es:test es')
+  def do_action(key = 'test.key', args = nil)
+    args ||= ['en:test en', 'es:test es']
+    @command.execute(key, *args)
   end
 
   before(:each) do
@@ -23,6 +24,18 @@ describe Localeapp::CLI::Add, "#execute(key, *translations)" do
     es_missing.size.should == 1
     es_missing['test.key'].locale.should == 'es'
     es_missing['test.key'].description.should == 'test es'
+  end
+
+  it "ignores badly formed arguments" do
+    with_configuration do
+      Localeapp.sender.stub!(:post_missing_translations)
+      do_action('test.key', ["en:this is fine", "esbad"])
+    end
+    Localeapp.missing_translations['en'].size.should == 1
+    Localeapp.missing_translations['es'].size.should == 0
+    Localeapp.missing_translations['esbad'].size.should == 0
+    @output.string.should include("Ignoring bad translation esbad")
+    @output.string.should include("format should be <locale>:<translation content>")
   end
 
   it "tells the sender to send the missing translations" do

71c9eaddcc3452a1565244c798abb8fd11c7d762
Michael Baudino <michael.baudino@alpine-lab.com>
2014-01-17 15:00:07 +0100

    Mimic Rails missing translation new display behaviour (wrap the translation key in a <span> element)

---
+++ lib/localeapp/rails/mimic_rails_missing_translation_display.rb
@@ -0,0 +1,15 @@
+# Rails 3.2.16 and 4.0.2 introduced a new way of displaying missing translation :
+# they now wrap them in a <span> element with useful class and title
+#
+# https://github.com/rails/rails/commit/78790e4bceedc632cb40f9597792d7e27234138a
+
+module Localeapp
+  module MimicRailsMissingTranslationDisplay
+    def self.call(exception, locale, key, options)
+      locale, key = super(exception, locale, key, options).split(', ')
+      "<span class=\"translation_missing\" title=\"translation missing: #{key}\">#{key} (#{locale})</span>"
+    end
+  end
+end
+
+Localeapp::ExceptionHandler.send(:include, ::Localeapp::MimicRailsMissingTranslationDisplay)

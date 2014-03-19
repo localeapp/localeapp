eb03f734caae9534209e6d0d05661871bfea298c
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-19 18:09:40 +0100

    Check timestamp age before calling the API

--- spec/localeapp/cli/update_spec.rb
+++ spec/localeapp/cli/update_spec.rb
@@ -2,17 +2,38 @@ require 'spec_helper'
 require 'localeapp/cli/update'
 
 describe Localeapp::CLI::Update, "#execute" do
-  before do
-    @output = StringIO.new
-    @updater = Localeapp::CLI::Update.new(:output => @output)
+  let(:output)  { StringIO.new }
+  let(:updater) { Localeapp::CLI::Update.new(:output => output) }
+  let(:poller)  { Localeapp::Poller.new }
+
+  before(:each) { Localeapp::Poller.stub(:new => poller) }
+
+  context "when timestamp is recent" do
+    before(:each) { poller.stub(:updated_at => Time.now.to_i - 60) }
+
+    it "creates a Poller and calls poll! on it" do
+      with_configuration do
+        poller.should_receive(:poll!)
+        updater.execute
+      end
+    end
   end
 
-  it "creates a Poller and calls poll! on it" do
-    with_configuration do
-      poller = Localeapp::Poller.new
-      poller.should_receive(:poll!)
-      Localeapp::Poller.should_receive(:new).and_return(poller)
-      @updater.execute
+  context "when timestamp is too old" do
+    before(:each) { poller.stub(:updated_at => 0) }
+
+    it "warns the user" do
+      with_configuration do
+        updater.execute
+        output.string.should include("Timestamp is missing or too old.")
+      end
+    end
+
+    it "does not even call the API" do
+      with_configuration do
+        poller.should_not_receive(:poll!)
+        updater.execute
+      end
     end
   end
 end

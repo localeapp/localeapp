9ca3e527b65a964b3066da19021250ad36cf1f96
Michael Baudino <michael.baudino@alpine-lab.com>
2015-12-20 11:06:01 +0400

    Move requires out the Localeapp::Routes module

--- lib/localeapp/routes.rb
+++ lib/localeapp/routes.rb
@@ -1,16 +1,16 @@
 require 'rack/utils'
 
+require "localeapp/routes/base"
+require "localeapp/routes/projects"
+require "localeapp/routes/translations"
+require "localeapp/routes/export"
+require "localeapp/routes/remove"
+require "localeapp/routes/rename"
+require "localeapp/routes/missing_translations"
+require "localeapp/routes/import"
+
 module Localeapp
   module Routes
     VERSION = 'v1'
-
-    require "localeapp/routes/base"
-    require "localeapp/routes/projects"
-    require "localeapp/routes/translations"
-    require "localeapp/routes/export"
-    require "localeapp/routes/remove"
-    require "localeapp/routes/rename"
-    require "localeapp/routes/missing_translations"
-    require "localeapp/routes/import"
   end
 end

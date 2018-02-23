30b31a9b97a91d0f63e273c3f25f6b9a630e2ab0
Leon Hooijer <leonhooijer@outlook.com>
2018-02-23 11:31:46 +0100

    Scope keys in ExceptionHandler messages

--- lib/localeapp/exception_handler.rb
+++ lib/localeapp/exception_handler.rb
@@ -2,16 +2,17 @@ module Localeapp
   class ExceptionHandler
     def self.call(exception, locale, key_or_keys, options)
       keys = Array(key_or_keys).map { |key| ERB::Util.html_escape(key.to_s) }
+      scoped_keys = keys.map { |key| [options[:scope], key].compact.join(".") }
       Localeapp.log(exception.message)
       # Which exact exception is set up by our i18n shims
       if exception.is_a? Localeapp::I18nMissingTranslationException
-        Localeapp.log("Detected missing translation for key(s) #{keys.inspect}")
+        Localeapp.log("Detected missing translation for key(s) #{scoped_keys.inspect}")
 
         keys.each do |key|
           Localeapp.missing_translations.add(locale, key, nil, options || {})
         end
 
-        [locale, keys].join(', ')
+        [locale, scoped_keys].join(', ')
       else
         Localeapp.log('Raising exception')
         raise
